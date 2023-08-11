f79dfbd280b40287bcbed4b37609564e95b5e67e
Afreza Dwi Zuliyanto <130138077+afreza52@users.noreply.github.com>
2023-08-11 17:28:58 +0700

    Update antrian_klinik.sql

--- antrian_klinik.sql
+++ antrian_klinik.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.0
+-- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 10 Agu 2023 pada 18.24
--- Versi server: 10.4.24-MariaDB
--- Versi PHP: 8.0.19
+-- Waktu pembuatan: 11 Agu 2023 pada 12.26
+-- Versi server: 10.4.21-MariaDB
+-- Versi PHP: 8.0.10
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,19 +24,6 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `admin`
---
-
-CREATE TABLE `admin` (
-  `id_admin` int(11) NOT NULL,
-  `username` varchar(50) NOT NULL,
-  `password` varchar(50) NOT NULL,
-  `created_at` datetime NOT NULL DEFAULT current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- --------------------------------------------------------
-
---
 -- Struktur dari tabel `antrian`
 --
 
@@ -46,15 +33,8 @@ CREATE TABLE `antrian` (
   `id_dokter` int(11) NOT NULL,
   `tanggal_antrian` date NOT NULL,
   `jam_antrian` time NOT NULL,
-  `status` enum('dalam antrian','selesai') NOT NULL
+  `status_antrian` varchar(25) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data untuk tabel `antrian`
---
-
-INSERT INTO `antrian` (`id_antrian`, `id_layanan`, `id_dokter`, `tanggal_antrian`, `jam_antrian`, `status`) VALUES
-(1, 1, 1, '2023-08-10', '22:40:00', 'dalam antrian');
 
 -- --------------------------------------------------------
 
@@ -64,17 +44,10 @@ INSERT INTO `antrian` (`id_antrian`, `id_layanan`, `id_dokter`, `tanggal_antrian
 
 CREATE TABLE `dokter` (
   `id_dokter` int(11) NOT NULL,
-  `nama_dokter` varchar(100) NOT NULL,
-  `spesialis` varchar(100) NOT NULL,
-  `id_jadwal` int(11) NOT NULL
+  `nama_dokter` varchar(50) NOT NULL,
+  `spesialis` varchar(50) NOT NULL,
+  `jadwal_dokter` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data untuk tabel `dokter`
---
-
-INSERT INTO `dokter` (`id_dokter`, `nama_dokter`, `spesialis`, `id_jadwal`) VALUES
-(1, 'example', 'example', 1);
 
 -- --------------------------------------------------------
 
@@ -84,18 +57,12 @@ INSERT INTO `dokter` (`id_dokter`, `nama_dokter`, `spesialis`, `id_jadwal`) VALU
 
 CREATE TABLE `jadwal` (
   `id_jadwal` int(11) NOT NULL,
+  `id_dokter` int(11) NOT NULL,
   `tanggal` date NOT NULL,
   `jam_mulai` time NOT NULL,
   `jam_selesai` time NOT NULL,
-  `status` enum('tersedia','tidak tersedia') NOT NULL
+  `status_ketersediaan` varchar(15) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data untuk tabel `jadwal`
---
-
-INSERT INTO `jadwal` (`id_jadwal`, `tanggal`, `jam_mulai`, `jam_selesai`, `status`) VALUES
-(1, '2023-08-10', '19:00:00', '03:00:00', 'tersedia');
 
 -- --------------------------------------------------------
 
@@ -105,16 +72,10 @@ INSERT INTO `jadwal` (`id_jadwal`, `tanggal`, `jam_mulai`, `jam_selesai`, `statu
 
 CREATE TABLE `layanan` (
   `id_layanan` int(11) NOT NULL,
-  `nama` varchar(50) NOT NULL,
-  `deskripsi` varchar(128) NOT NULL
+  `nama_layanan` varchar(50) NOT NULL,
+  `deskripsi_layanan` text NOT NULL,
+  `durasi_layanan` int(3) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data untuk tabel `layanan`
---
-
-INSERT INTO `layanan` (`id_layanan`, `nama`, `deskripsi`) VALUES
-(1, 'example', 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy te');
 
 -- --------------------------------------------------------
 
@@ -124,19 +85,11 @@ INSERT INTO `layanan` (`id_layanan`, `nama`, `deskripsi`) VALUES
 
 CREATE TABLE `pengguna` (
   `id_pengguna` int(11) NOT NULL,
-  `pengguna` varchar(50) NOT NULL,
-  `alamat` varchar(100) NOT NULL,
-  `email` varchar(50) NOT NULL,
-  `no_hp` varchar(15) NOT NULL,
-  `informasi` varchar(128) DEFAULT NULL
+  `nama_pengguna` varchar(50) NOT NULL,
+  `email` varchar(30) NOT NULL,
+  `no_telp` varchar(15) NOT NULL,
+  `informasi` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data untuk tabel `pengguna`
---
-
-INSERT INTO `pengguna` (`id_pengguna`, `pengguna`, `alamat`, `email`, `no_hp`, `informasi`) VALUES
-(1, 'admin', 'no address', 'no email', '088888888888', 'no information');
 
 -- --------------------------------------------------------
 
@@ -147,47 +100,34 @@ INSERT INTO `pengguna` (`id_pengguna`, `pengguna`, `alamat`, `email`, `no_hp`, `
 CREATE TABLE `umpan_balik` (
   `id_umpan_balik` int(11) NOT NULL,
   `id_antrian` int(11) NOT NULL,
-  `komentar` varchar(128) NOT NULL,
-  `rating` enum('1/5','2/5','3/5','4/5','5/5') NOT NULL
+  `komentar` text NOT NULL,
+  `rating` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data untuk tabel `umpan_balik`
---
-
-INSERT INTO `umpan_balik` (`id_umpan_balik`, `id_antrian`, `komentar`, `rating`) VALUES
-(1, 1, 'example', '4/5');
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indeks untuk tabel `admin`
---
-ALTER TABLE `admin`
-  ADD PRIMARY KEY (`id_admin`);
-
---
 -- Indeks untuk tabel `antrian`
 --
 ALTER TABLE `antrian`
   ADD PRIMARY KEY (`id_antrian`),
-  ADD KEY `id_dokter` (`id_dokter`),
-  ADD KEY `id_layanan` (`id_layanan`);
+  ADD KEY `id_layanan` (`id_layanan`),
+  ADD KEY `id_dokter` (`id_dokter`);
 
 --
 -- Indeks untuk tabel `dokter`
 --
 ALTER TABLE `dokter`
-  ADD PRIMARY KEY (`id_dokter`),
-  ADD KEY `id_jadwal` (`id_jadwal`);
+  ADD PRIMARY KEY (`id_dokter`);
 
 --
 -- Indeks untuk tabel `jadwal`
 --
 ALTER TABLE `jadwal`
-  ADD PRIMARY KEY (`id_jadwal`);
+  ADD PRIMARY KEY (`id_jadwal`),
+  ADD KEY `id_dokter` (`id_dokter`);
 
 --
 -- Indeks untuk tabel `layanan`
@@ -213,46 +153,40 @@ ALTER TABLE `umpan_balik`
 --
 
 --
--- AUTO_INCREMENT untuk tabel `admin`
---
-ALTER TABLE `admin`
-  MODIFY `id_admin` int(11) NOT NULL AUTO_INCREMENT;
-
---
 -- AUTO_INCREMENT untuk tabel `antrian`
 --
 ALTER TABLE `antrian`
-  MODIFY `id_antrian` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id_antrian` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT untuk tabel `dokter`
 --
 ALTER TABLE `dokter`
-  MODIFY `id_dokter` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id_dokter` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT untuk tabel `jadwal`
 --
 ALTER TABLE `jadwal`
-  MODIFY `id_jadwal` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id_jadwal` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT untuk tabel `layanan`
 --
 ALTER TABLE `layanan`
-  MODIFY `id_layanan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id_layanan` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT untuk tabel `pengguna`
 --
 ALTER TABLE `pengguna`
-  MODIFY `id_pengguna` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id_pengguna` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT untuk tabel `umpan_balik`
 --
 ALTER TABLE `umpan_balik`
-  MODIFY `id_umpan_balik` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id_umpan_balik` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
@@ -262,14 +196,14 @@ ALTER TABLE `umpan_balik`
 -- Ketidakleluasaan untuk tabel `antrian`
 --
 ALTER TABLE `antrian`
-  ADD CONSTRAINT `antrian_ibfk_1` FOREIGN KEY (`id_dokter`) REFERENCES `dokter` (`id_dokter`) ON DELETE CASCADE,
-  ADD CONSTRAINT `antrian_ibfk_2` FOREIGN KEY (`id_layanan`) REFERENCES `layanan` (`id_layanan`) ON DELETE CASCADE;
+  ADD CONSTRAINT `antrian_ibfk_1` FOREIGN KEY (`id_layanan`) REFERENCES `layanan` (`id_layanan`) ON DELETE CASCADE,
+  ADD CONSTRAINT `antrian_ibfk_2` FOREIGN KEY (`id_dokter`) REFERENCES `dokter` (`id_dokter`) ON DELETE CASCADE;
 
 --
--- Ketidakleluasaan untuk tabel `dokter`
+-- Ketidakleluasaan untuk tabel `jadwal`
 --
-ALTER TABLE `dokter`
-  ADD CONSTRAINT `dokter_ibfk_1` FOREIGN KEY (`id_jadwal`) REFERENCES `jadwal` (`id_jadwal`);
+ALTER TABLE `jadwal`
+  ADD CONSTRAINT `jadwal_ibfk_1` FOREIGN KEY (`id_dokter`) REFERENCES `dokter` (`id_dokter`) ON DELETE CASCADE;
 
 --
 -- Ketidakleluasaan untuk tabel `umpan_balik`
